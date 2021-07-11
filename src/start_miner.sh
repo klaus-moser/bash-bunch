2efe719ddf5de67325608b20b2370151bd3e4bf8
klaus-moser <don_kanaille@posteo.de>
2021-07-11 15:24:58 +0200

    Mini script for miner

--- src/start_miner.sh
+++ src/start_miner.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
 
+cd ~/duino-coin;
 
-echo "Clone/Update Miner";
+echo $(date -u) "Start Miner" >> start_miner.log;
 
-
-# Check if folder still exists
-
-if [ -d duino-coin ]
-then
-	cd ~/duino-coin;
-	git pull;
-else
-	git clone https://github.com/revoxhere/duino-coin.git;
-	cd ~/duino-coin;
-fi
-
-
-sleep 3;
-
-
-# Check venv folder
-
-if [ -d venv ]
-then
-	source venv/bin/activate;
-else
-	virtualenv venv;
-	source venv/bin/activate;
-fi
-
-
-# If youre in venv install/update packages
-
-if [[ "$VIRTUAL_ENV" != "" ]]
-then
-	pip install -r "requirements.txt";
-
-	echo "Start Miner";
-	python3 PC_Miner.py
-else
-	echo "Unable to activate venv + install packages!" >> error.log;
-fi
+python3 PC_Miner.py;
